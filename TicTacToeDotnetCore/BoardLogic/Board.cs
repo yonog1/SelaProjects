ee0e74bbe02fa055cfad6465c7272a0301b34031
yonog1 <yonatangoldin69@gmail.com>
2023-08-19 02:46:51 +0300

    initial commit for tic tac toe dotnet core version

---
+++ TicTacToeDotnetCore/BoardLogic/Board.cs
@@ -0,0 +1,72 @@
+﻿namespace BoardLogic
+{
+    public class Board
+    {
+        private char[,] Grid { get; set; } = { { '.', '.', '.' }, { '.', '.', '.' }, { '.', '.', '.' } };
+        public short numOfTurns { get; private set; } = 0;
+        public bool turn { get; private set; } = true;
+        public bool winnerFound { get; set; } = false;
+        public Board()
+        {
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 0; i < Grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < Grid.GetLength(1); j++)
+                {
+                    Console.Write($" {Grid[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void AddToGrid(int x, int y)
+        {
+            Grid[x, y] = turn ? 'X' : 'O';
+            numOfTurns++;
+            turn = !turn;
+        }
+
+        public bool IsPlaceAvailable(int x, int y)
+        {
+            if (Grid[x, y] == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckWinner()
+        {
+            // Rows check
+            for (int i = 0; i < 3; i++)
+            {
+                if (Grid[i, 0] == Grid[i, 1] && Grid[i, 1] == Grid[i, 2] && Grid[i, 0] != '.')
+                {
+                    return true;
+                }
+            }
+
+            // Columns check
+            for (int i = 0; i < 3; i++)
+            {
+                if (Grid[0, i] == Grid[1, i] && Grid[1, i] == Grid[2, i] && Grid[0, i] != '.')
+                {
+                    return true;
+                }
+            }
+
+            // Diagonals check
+            if ((Grid[0, 0] == Grid[1, 1] && Grid[1, 1] == Grid[2, 2] && Grid[0, 0] != '.') ||
+                (Grid[0, 2] == Grid[1, 1] && Grid[1, 1] == Grid[2, 0] && Grid[0, 2] != '.'))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ TicTacToeDotnetCore/TicTacToeCore/Program.cs
@@ -0,0 +1,69 @@
+﻿using BoardLogic;
+
+namespace TicTacToeCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Board game = new();
+
+            while (game.numOfTurns < 9) // cant be more than 9 turns, board is 3x3
+            {
+                Console.Clear();
+                if (game.numOfTurns >= 5) // check winner from 5 turns, there cant be a winner before
+                {
+                    if (game.CheckWinner())
+                    {
+                        Console.WriteLine($"Winner is {(game.turn ? 'O' : 'X')}"); // print the symbol played before current since the winner would have made the winning move before the one currently set
+                        game.winnerFound = true;
+                        break;
+                    }
+                    else if (game.numOfTurns == 9)
+                    {
+                        Console.WriteLine("Its a draw!");
+                        break;
+                    }
+                }
+
+                int row;
+                int column;
+                // Get user input from console
+                while (true)
+                {
+                    game.PrintBoard();
+                    do
+                    {
+                        Console.WriteLine("Enter the row number [1-3]:");
+                        row = (int.TryParse(Console.ReadLine(), out row) ? row : -1) - 1; // Decrease -1 to fit array indexes
+                    }
+                    while (row < 0 || row > 2);
+
+                    do
+                    {
+                        Console.WriteLine("Enter the column number [1-3]:");
+                        column = (int.TryParse(Console.ReadLine(), out column) ? column : -1) - 1; // Decrease -1 to fit array indexes
+                    }
+                    while (column < 0 || column > 2);
+
+                    if (game.IsPlaceAvailable(row, column))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"This position ({row + 1},{column + 1}) is already taken, please choose another one. Press any key to continue:");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                game.AddToGrid(row, column);
+            }
+            if (!game.winnerFound)
+            {
+                Console.WriteLine("Draw");
+            }
+        }
+    }
+}
